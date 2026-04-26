6aa7ce9fe2377f89d20efec7d33bca0aaf67b3e9
fwernborg-png <fwernborg@gmail.com>
2026-04-26 14:19:24 +0000

    fix vite config

--- lastbilsapp/vite.config.js
+++ lastbilsapp/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lastbilsutmaningen/',
-})import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
   base: '/',
 })
